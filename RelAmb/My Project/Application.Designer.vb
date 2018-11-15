62b63e5c31cd99617b3d909287752335ff871971
Thomas Verliefde <tverliefde@hotmail.com>
2018-11-15 11:55:40 +0100

    Name/Folder Change

--- RelAmb/My Project/Application.Designer.vb
+++ RelAmb/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Sandbox.mainForm
+            Me.MainForm = Global.RelAmb.mainForm
         End Sub
     End Class
 End Namespace
